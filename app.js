28c77126dc4c74d9b7d9d84eaae97eeb8d3664d6
Fangjie Yao <t-fay@microsoft.com>
2016-04-15 23:01:16 +0800

    path problem fixed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var blog = require('./router/blog.js');
 var login = require('./router/login.js')
 
@@ -6,13 +7,14 @@ var app = express();
 
 app.set('view engine', 'ejs'); //use ejs(js syntax) as template engine, render() will search views directory
 
-app.use('/views', express.static(__dirname + '/views')); //views
-app.use('/static', express.static(__dirname + '/static')); //static resources
+app.set('views', path.join(__dirname, 'views')); //views
+app.use(express.static(path.join(__dirname, 'static'))); //static resources
+
 app.use('/blog', blog);
 app.use('/admin', login);
 
 app.get('/', function(req, res) {
     res.render('index');
-})
+});
 
 app.listen(3000);
