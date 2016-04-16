8e334f3399f485c9182ba1fbd77fcfe74f5d7c49
Fangjie Yao <t-fay@microsoft.com>
2016-04-16 12:55:34 +0800

    add metadata, add bootstrap

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var path = require('path');
+
+var index = require('./router/index.js');
 var blog = require('./router/blog.js');
 var login = require('./router/login.js')
 
 var app = express();
 
+//config
 app.set('view engine', 'ejs'); //use ejs(js syntax) as template engine, render() will search views directory
-
 app.set('views', path.join(__dirname, 'views')); //views
 app.use(express.static(path.join(__dirname, 'static'))); //static resources
 
+//router
+app.use('/', index);
 app.use('/blog', blog);
 app.use('/admin', login);
-
-app.get('/', function(req, res) {
-    res.render('index');
-});
 
 app.listen(3000);
