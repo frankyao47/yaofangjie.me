089737dadc356b0af712bf1d40447f3eaf441075
Fangjie Yao <t-fay@microsoft.com>
2016-04-18 15:55:33 +0800

    add body-parser and test

--- router/blog.js
+++ router/blog.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var router = express.Router();
+
+//bodyParser middleware
+router.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 router.get('/', function(req, res) {
     res.send('blog');
 });
+
+router.route('/create')
+    .get(function(req, res, next) {
+        res.render('blog/create');
+    })
+    .post(function(req, res, next) {
+        console.log(req.body);
+        res.send(req.body);
+    });
+
 
 module.exports = router;
